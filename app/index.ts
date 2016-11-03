23be51f5595567397ea0e222e29809af40cc0985
OverG <antonin.carlin@gmail.com>
2016-11-03 10:41:52 +0100

    Ajout du fichiers config dans l'index

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,6 @@
 import * as services from './Services/index.service';
 export { AppComponent } from './Composents/app.component';
+export {routes} from './routes';
 
 const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
 
