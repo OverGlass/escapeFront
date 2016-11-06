c509c35b1e4b6b3458bec0901e6fa522c4254d8d
OverG <antonin.carlin@gmail.com>
2016-11-06 17:03:00 +0100

    Importation du service User

--- app/Services/index.ts
+++ app/Services/index.ts
@@ -3,3 +3,4 @@ export {AuthService} from './auth.service';
 export  {LoggedInGuard} from './login-in.guard';
 export {Animations} from './animation';
 export {GeolocationService} from './geolocalisation';
+export {UserService} from './user.service';
