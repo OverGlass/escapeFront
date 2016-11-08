366e8d9afc33c811dbac25fbeb8385cf0acc7792
OverG <antonin.carlin@gmail.com>
2016-11-08 16:04:12 +0100

    Ajout des UI events

---
+++ app/Composents/events/ui/event-tab.ts
@@ -0,0 +1,19 @@
+import {Component} from "@angular/core";
+@Component({
+    selector:'event-tab',
+    template:
+    `
+        <div class="small-12 medium-6 columns">
+          <div class="event">
+            <span>sport<p>dateTime | Par @sercreateur</p></span>
+            <div class="qt-people">10</div>
+          </div>
+          <div class="event"></div>
+        </div>
+`,
+
+})
+
+export class EventTab {
+
+}
